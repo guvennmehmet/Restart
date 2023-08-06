11ea5ada450c6830d7a188ead47b4761a21be153
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-06 13:50:52 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mehmet Güven on 6.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
