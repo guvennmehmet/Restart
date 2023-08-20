0d5f5090cbc64405a8868f8dc74db06962a15961
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-20 17:06:58 +0300

    Added the complex swiftui drag gesture

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -12,6 +12,9 @@ struct OnboardingView: View {
     
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffSet: CGFloat = 0
+    
     // MARK: - BODY
     
     var body: some View {
@@ -79,7 +82,7 @@ struct OnboardingView: View {
                     HStack {
                         Capsule()
                             .fill(Color("ColorRed"))
-                            .frame(width: 80)
+                            .frame(width: buttonOffSet + 80)
                         
                         Spacer()
                     }
@@ -98,15 +101,29 @@ struct OnboardingView: View {
                         }
                         .foregroundColor(.white)
                         .frame(width: 80, height: 80, alignment: .center)
-                        .onTapGesture {
-                            isOnboardingViewActive = false
-                        }
+                        .offset(x: buttonOffSet)
+                        .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                if gesture.translation.width > 0 && buttonOffSet <= buttonWidth - 80 {
+                                    buttonOffSet = gesture.translation.width
+                                }
+                            }
+                            .onEnded { _ in
+                                if buttonOffSet > buttonWidth / 2 {
+                                    buttonOffSet = buttonWidth - 80
+                                    isOnboardingViewActive = false
+                                } else {
+                                    buttonOffSet = 0
+                                }
+                            }
+                        ) //: GESTURE
                         
                         Spacer()
                     } //: HSTACK
                     
                 } //: FOOTER
-                .frame(height: 80, alignment: .center)
+                .frame(width: buttonWidth, height: 80, alignment: .center)
                 .padding()
             } //: VSTACK
         } //: ZSTACK
