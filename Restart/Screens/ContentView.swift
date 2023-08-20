9b10f58f7077666a9b6f4613d71964b33eb47916
Mehmet Güven <guvennmehmet37@gmail.com>
2023-08-20 17:30:28 +0300

    Added the swiftui animation

--- Restart/Screens/ContentView.swift
+++ Restart/Screens/ContentView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
-    
-    var body: some View {
-        ZStack {
-            if isOnboardingViewActive {
-                OnboardingView()
-            } else {
-                HomeView()
-            }
-        }
+  @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+  
+  var body: some View {
+    ZStack {
+      if isOnboardingViewActive {
+        OnboardingView()
+      } else {
+        HomeView()
+      }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
